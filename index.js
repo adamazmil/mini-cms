555915953ff4673fc4836d22d33fb43711136473
Adam Azmil <muhd.adam1@gmail.com>
2022-03-05 19:48:00 +0800

    feat(VideoRouter): restricts delete and edit with Auth0

--- index.js
+++ index.js
@@ -3,17 +3,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const { auth } = require("express-openid-connect");
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 var video = require("./routes/VideoRouter.js");
+
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
+app.use(auth(config));
 app.get("/", (request, response) => {
   response.json({ info: "Video serving cms webapp" });
 });
 app.use("/", video);
+
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
