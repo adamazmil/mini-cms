85c8d1eb0467e32f08670934594a1d8130cd4fd9
Adam Azmil <muhd.adam1@gmail.com>
2022-03-05 16:00:55 +0800

    docs(VideoRouter): adds comments for REST

--- routes/VideoRouter.js
+++ routes/VideoRouter.js
@@ -2,10 +2,17 @@ var express = require("express");
 var router = express.Router();
 const db = require("../queries");
 
+//VIDEO ROUTES
+
+//GET request for list of all video
 router.get("/videos", db.getVideos);
+//GET request for one Video
 router.get("/videos/:id", db.getVideosById);
+//POST request for creating video
 router.post("/videos", db.createVideo);
+//PUT REQUEST TO UPDATE VIDEO
 router.put("/videos/:id", db.updateVideo);
+//DELETE REQUST TO DELETE VIDEO
 router.delete("/videos/:id", db.deleteVideo);
 
 module.exports = router;
