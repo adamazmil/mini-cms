005de932edf33b77f4f4024890bd3067a0e657ba
Adam Azmil <muhd.adam1@gmail.com>
2022-03-06 21:12:17 +0800

    feat(app): adds GET call for video from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [videos, setVideos] = React.useState([]);
+  const getVideos = async () => {
+    await fetch("api/videos")
+      .then((res) => res.json())
+      .then((data) => setVideos(data));
+  };
+  const ListVideo = ({ id, title }) => (
+    <li>
+      {title} &mdash; {id}
+    </li>
+  );
+  const ListVideos = ({ videos }) => (
+    <ul>
+      {videos.map((props) => (
+        <ListVideo key={props.id} {...props} />
+      ))}
+    </ul>
+  );
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p onClick={getVideos}>Click to call api</p>
+        <ListVideos videos={videos} />
       </header>
     </div>
   );
